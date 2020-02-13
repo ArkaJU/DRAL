279f3c12e750e30e1b07a77e6ec0fc4e0d01fd08
Arka Saha <40143090+ArkaJU@users.noreply.github.com>
2020-02-13 09:48:49 +0530

    Created the file

---
+++ distances.py
@@ -0,0 +1,27 @@
+def mahalanobis_dist_from_features(outputs):
+    batch_size = outputs.size(0)
+
+    # Compute Mahalanobis distance matrix.
+    magnitude = (outputs ** 2).sum(1).expand(batch_size, batch_size)
+    squared_matrix = outputs.mm(torch.t(outputs))
+    mahalanobis_distances = F.relu(magnitude + torch.t(magnitude) - 2 * squared_matrix).sqrt()
+    return mahalanobis_distances
+
+def mahalanobis_dist_from_vectors(x, y):
+    
+    X = T.cat((x, y))
+    X = mahalanobis_dist_from_features(X)
+    #print(X)
+    return X[0][1]                              #bad coding but still
+
+def calculate_similarity(features):                   #features->concatenate query and g features   
+    x = mahalanobis_dist_from_features(features)
+    #print(x)
+    #print("*************************************************")
+    x = x/T.max(x)
+    #print(x)
+    #print("*************************************************")
+    matrix = 1 - x
+    #print(matrix)
+    matrix.fill_diagonal_(0)                                       #look into this
+    return matrix
